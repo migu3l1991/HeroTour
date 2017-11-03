9d47c1434defac9d6a754321bea3ed1d120bfcc4
Miguel <migu3l1991@hotmail.com>
2017-11-03 03:18:53 -0500

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule }     from './app-routing/app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent,
+    HeroSearchComponent
+  ],
+  providers: [ 
+    HeroService
+   ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
